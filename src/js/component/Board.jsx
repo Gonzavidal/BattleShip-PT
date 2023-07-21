b5668561bd4d112a5595445795918a712a18413e
Gonzavidal <gvidal1992@gmail.com>
2023-07-21 23:02:03 +0000

    Trabajando lógica enesimavez

--- src/js/component/Board.jsx
+++ src/js/component/Board.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/index.css";
 
-const BOARD_SIZE = 10;  // Disposición de tablero
+const BOARD_SIZE = 10; // Disposición de tablero
 
 const initialState = {
   playerBoard: Array.from({ length: BOARD_SIZE }, () =>
@@ -10,24 +10,27 @@ const initialState = {
   computerBoard: Array.from({ length: BOARD_SIZE }, () =>
     Array.from({ length: BOARD_SIZE }, () => null)
   ),
-  selectedCells: [],
-  selectedEmptyCells: [],
+  selectedCellsPlayer: [], // Agregar selectedCellsPlayer
+  selectedEmptyCellsPlayer: [], // Agregar selectedEmptyCellsPlayer
+  selectedCellsComputer: [], // Agregar selectedCellsComputer
+  selectedEmptyCellsComputer: [], // Agregar selectedEmptyCellsComputer
   isGameOver: false,
   winner: null,
-  isPlayerTurn: true
+  isPlayerTurn: true,
 };
 
-const shipTypes = [  // Tipos de barcos y su longitud
+const shipTypes = [
+  // Tipos de barcos y su longitud
   { name: "Carrier", size: 5 },
   { name: "Battleship", size: 4 },
   { name: "Cruiser", size: 3 },
   { name: "Submarine", size: 3 },
-  { name: "Destroyer", size: 2 }
+  { name: "Destroyer", size: 2 },
 ];
 
 const getRandomPosition = () => Math.floor(Math.random() * BOARD_SIZE);
 
-const App = () => {
+const Board = () => {
   const [state, setState] = useState(initialState);
 
   const placeShipsRandomly = (board) => {
@@ -67,7 +70,8 @@ const App = () => {
     return updatedBoard;
   };
 
-  const handleMove = (row, col, board) => {  // Disparos Player
+  const handlePlayerMove = (row, col, board) => {
+    // Disparos Player
     if (state.isGameOver || board[row][col].hit) {
       return;
     }
@@ -87,19 +91,20 @@ const App = () => {
         computerBoard: updatedBoard,
         isGameOver,
         winner: isGameOver ? "Player" : null,
-        isPlayerTurn: false
+        isPlayerTurn: false,
       }));
     } else {
       const selectedCell = { row, col };
       setState((prevState) => ({
         ...prevState,
-        selectedCells: [...prevState.selectedCells, selectedCell],
-        isPlayerTurn: false
+        selectedCellsPlayer: [...prevState.selectedCellsPlayer, selectedCell],
+        isPlayerTurn: false,
       }));
     }
   };
 
-  const handleComputerMove = () => { // Disparos CPU
+  const handleComputerMove = () => {
+    // Disparos CPU
     if (state.isGameOver || state.isPlayerTurn) {
       return;
     }
@@ -129,7 +134,7 @@ const App = () => {
       playerBoard: updatedPlayerBoard,
       isGameOver,
       winner: isGameOver ? "Computer" : null,
-      isPlayerTurn: true
+      isPlayerTurn: true,
     }));
   };
 
@@ -137,7 +142,7 @@ const App = () => {
     setState((prevState) => ({
       ...prevState,
       playerBoard: placeShipsRandomly(prevState.playerBoard),
-      computerBoard: placeShipsRandomly(prevState.computerBoard)
+      computerBoard: placeShipsRandomly(prevState.computerBoard),
     }));
   }, []);
 
@@ -148,44 +153,42 @@ const App = () => {
       });
 
       return () => clearTimeout(timer);
-    } else {
-      setState((prevState) => ({
-        ...prevState,
-        selectedEmptyCells: []
-      }));
     }
   }, [state.isPlayerTurn]);
 
-  const handleCellClick = (row, col) => {  // Función disparo celdas
+  const handleCellClick = (row, col) => {
+    // Función disparo celdas
     if (state.isPlayerTurn) {
       const clickedCell = state.computerBoard[row][col];
-      if (clickedCell === null || (clickedCell !== null && !clickedCell.hit)) {
-        handleMove(row, col, state.computerBoard);
-      } else {
-        const selectedCell = { row, col };
-        setState((prevState) => ({
-          ...prevState,
-          selectedCells: [...prevState.selectedCells, selectedCell]
-        }));
-      }
-
       const isEmptyCell = clickedCell === null;
-      const isSelectedEmptyCell = state.selectedEmptyCells.some(
-        (selectedCell) => selectedCell.row === row && selectedCell.col === col
-      );
 
-      if (isEmptyCell && !isSelectedEmptyCell) {
-        const selectedEmptyCell = { row, col };
-        setState((prevState) => ({
-          ...prevState,
-          selectedEmptyCells: [...prevState.selectedEmptyCells, selectedEmptyCell]
-        }));
+      if (isEmptyCell) {
+        const isSelectedEmptyCell = state.selectedEmptyCellsComputer.some(
+          (selectedCell) => selectedCell.row === row && selectedCell.col === col
+        );
+
+        if (!isSelectedEmptyCell) {
+          const updatedBoard = state.computerBoard.map((row) => [...row]);
+          updatedBoard[row][col] = { miss: true };
+
+          setState((prevState) => ({
+            ...prevState,
+            computerBoard: updatedBoard,
+            isPlayerTurn: false,
+            selectedEmptyCellsComputer: [
+              ...prevState.selectedEmptyCellsComputer,
+              { row, col },
+            ],
+          }));
+        }
+      } else if (!clickedCell.hit) {
+        handlePlayerMove(row, col, state.computerBoard);
       }
     }
   };
 
   return (
-    <div className="container">  
+    <div className="container">
       <h1>Battleship</h1>
       {state.isGameOver && (
         <div className="message">
@@ -193,53 +196,61 @@ const App = () => {
           {state.winner === "Computer" && <h2>Game Over! Computer won!</h2>}
         </div>
       )}
-      
-      <div className="game"> 
-        <div className="player-board"> 
+
+      <div className="game">
+        <div className="player-board">
           <h3>Player</h3>
-          <div className="board">
-            {state.playerBoard.map((row, rowIndex) => // Tablero jugador
+          <div className="player">
+            {state.playerBoard.map((row, rowIndex) =>
               row.map((cell, colIndex) => {
                 let cellClass = "";
-                        if (cell !== null) {
-                          cellClass = cell.hit ? "hit" : "ship";
-                        }
-                        if (
-                          state.selectedCells.some(
-                            (selectedCell) => selectedCell.row === rowIndex && selectedCell.col === colIndex
-                          )
-                        ) {
-                          cellClass = "miss";
-                        }
-                        if (
-                          state.selectedEmptyCells.some(
-                            (selectedCell) => selectedCell.row === rowIndex && selectedCell.col === colIndex
-                          )
-                        ) {
-                          cellClass = "empty-cell";
-                        }
-                        return (
-                          <div
-                            key={`${rowIndex}-${colIndex}`}
-                            className={`cell ${cellClass}`}
-                          />
-                        );
+
+                if (cell !== null) {
+                  cellClass = cell.hit ? "hit" : "ship";
+                } else if (
+                  state.selectedCellsPlayer.some(
+                    (selectedCell) => selectedCell.row === rowIndex && selectedCell.col === colIndex
+                  )
+                ) {
+                  cellClass = "computer-miss";
+                } else if (
+                  state.selectedEmptyCellsPlayer.some(
+                    (selectedCell) => selectedCell.row === rowIndex && selectedCell.col === colIndex
+                  )
+                ) {
+                  cellClass = "miss"; // Usar la clase "miss" para celdas vacías
+                }
+
+                return (
+                  <div
+                    key={`${rowIndex}-${colIndex}`}
+                    className={`cell ${cellClass}`}
+                  />
+                );
               })
             )}
           </div>
         </div>
         <div className="computer-board">
           <h3>CPU</h3>
-          <div className="board">
-            {state.computerBoard.map((row, rowIndex) => // Tablero CPU
+          <div className="cpu">
+            {state.computerBoard.map((row, rowIndex) =>
               row.map((cell, colIndex) => {
                 let cellClass = "";
                 if (cell !== null) {
                   cellClass = cell.hit ? "hit" : "ship";
-                } else if (state.selectedCells.some(
-                  (selectedCell) => selectedCell.row === rowIndex && selectedCell.col === colIndex
-                )) {
-                  cellClass = "miss";
+                } else if (
+                  state.selectedCellsComputer.some(
+                    (selectedCell) => selectedCell.row === rowIndex && selectedCell.col === colIndex
+                  )
+                ) {
+                  cellClass = "computer-miss";
+                } else if (
+                  state.selectedEmptyCellsComputer.some(
+                    (selectedCell) => selectedCell.row === rowIndex && selectedCell.col === colIndex
+                  )
+                ) {
+                  cellClass = "miss"; // Usar la clase "miss" para celdas vacías
                 }
 
                 return (
@@ -258,4 +269,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Board;
